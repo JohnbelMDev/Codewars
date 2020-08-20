406d3848f0806a1cc902c7af9d5c79b34fad6bab
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-08-20 10:14:27 -0400

    complete codewars

---
+++ Arrayplusarray/index..js
@@ -0,0 +1,13 @@
+
+//declare an annoymous function with two parameter store in arrayPlusArray 
+var arrayPlusArray =function (arr1, arr2) {
+ var sum=0;
+ for (var i=0; i< arr1.length;i++)
+     sum+=arr1[i];
+ for (var secondArray=0; secondArray< arr2.length;secondArray++)
+      sum+=arr2[secondArray];
+
+   return sum;
+
+
+}
