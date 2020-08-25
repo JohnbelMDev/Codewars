919647c3e169f27b1d441f596c3c63818b43bd2a
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-08-25 09:50:15 -0400

    not complete

---
+++ Are you playing banjo/index.js
@@ -0,0 +1,13 @@
+function areYouPlayingBanjo(name) {
+
+  if(name[0]==="R" || name[0].toLowerCase() === ‘r’)
+    {
+      name="plays banjo";
+
+
+    }
+  else{
+    name="does not play banjo";
+   return name;
+    }
+}
