d4e8a9613232467e24c5d7b858c965f7de7d9e11
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-08-27 22:17:48 -0400

    complete

---
+++ simple calculator/index.js
@@ -0,0 +1,16 @@
+function calculator(a,b,sign){
+
+  if ((typeof (a) != 'number') || (typeof (b) != 'number'))
+    return "unknown value";
+
+  if (sign === "+")
+    return a + b;
+  else if (sign === "-")
+    return a - b;
+  else if (sign === "/")
+    return a / b;
+  else if (sign === "*")
+    return a * b;
+  else
+    return "unknown value";
+}
