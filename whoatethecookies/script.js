a7a6b19c3bb1d8a1f6309c1923551c86fdecef83
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-08-31 23:55:16 -0400

    who ate the cookies. complete

---
+++ whoatethecookies/script.js
@@ -0,0 +1,9 @@
+function cookie(x){
+  // using typof to indicate the type 
+  if (typeof x === 'string') {return 'Who ate the last cookie? It was Zach!'
+    }
+  if (typeof x === 'number'){ return 'Who ate the last cookie? It was Monica!'
+  return 'Who ate the last cookie? It was the dog!'
+}
+
+}
