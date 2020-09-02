494aa37f60923521d5b11987d1fc9f68845a8bdc
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-09-02 10:30:47 -0400

    complete was grabbing the wrong value

--- 6kyu/Multiples of or 5/script.js
+++ 6kyu/Multiples of or 5/script.js
@@ -2,14 +2,13 @@ function solution(number){
   var sum =0;
 
   for(var i=0; i<10; i++){
-    if(number%3==0 || number%5==0){
-      sum +=1;
-      return sum;
+    if(i%3==0 || i%5==0){
+      sum +=i;
 
   }
-    else if(number<0){
-      return -1;
-    }
+
   }
+   return sum;
+
 
 }
