9c559ef465e33fead50495f9822b449c7c37cdab
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-09-21 17:33:12 -0400

    commit

---
+++ 7_kyu/splitbyvalue/index.js
@@ -0,0 +1,10 @@
+function splitByValue(k, elements){
+  //   value is checking wheter or not if a value is less than k
+  let value = elements.filter(words => words < k)
+// value is checking wheter or not if a value is greater than k
+  let value2 = elements.filter(words => words >=k)
+  console.log(value,value2 )
+
+
+  return (value.concat(value2))
+}
