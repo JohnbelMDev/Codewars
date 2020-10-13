4e055f0ded67ad579dd5b5512535921f3ac1fcae
Johnbel <johnbel@Johnbels-MacBook-Pro.local>
2020-10-13 06:56:23 -0400

    commit

---
+++ 7_kyu/Pluck/script.js
@@ -0,0 +1,1 @@
+// Implement a function which takes a sequence of objects and a property name, and returns a sequence containing the named property of each object.
---
+++ 7_kyu/survive.js
@@ -0,0 +1,10 @@
+function hero(bullets, dragons){
+//Get Coding!
+  var num = bullets/dragons;
+  if(num<2){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
