6dee59bda4ffa23e2353101bf53457ab21637aaf
johnbelmdev <johnbel458@gmail.com>
2020-11-14 20:01:51 -0500

    commit

---
+++ 8_Kyu /For UFC Fans (Total Beginners): Conor McGregor vs George Saint Pierre/index.js
@@ -0,0 +1,13 @@
+var quote = function(fighter) {
+  // your code here
+  if(fighter==="George Saint Pierre" || fighter ==='george saint pierre'){
+    return "I am not impressed by your performance."
+
+  }
+  else if (fighter==='Conor McGregor' || fighter ==='conor mcgregor'){
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!"
+  }
+  else {
+    return "It's a tie"
+  }
+};
---
+++ 8_Kyu /Kata Example Twist/index.js
@@ -0,0 +1,5 @@
+// add the value "codewars" to the websites array 1,000 times
+var websites = []
+while(websites.length<1000){
+  websites.push("codewars")
+}
