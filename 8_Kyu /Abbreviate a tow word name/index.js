e4a4ec6ca3fe4288e2446769e1cdf87f890d9d8d
johnbelmdev <johnbel458@gmail.com>
2021-01-12 21:09:31 -0500

    new codewar

--- 8_Kyu /Abbreviate a tow word name/index.js
+++ 8_Kyu /Abbreviate a tow word name/index.js
@@ -1,12 +1,16 @@
-function abbrevName(a,x){
+//Taking a string as a parameter
+//return string with only initials
+//Same Harris
+    //S.H
+
+function abbrevName(name){
 
     // code away
-    if(a.includes(x)){
-      return true
-    }
-    else{
-      return false;
-    }
-
-
+    let splitString = name.split(' ')
+    let firstInitials = splitString[0].split('')[0].toUpperCase()
+    let secoInitials = splitString[1].split('')[0].toUpperCase()
+    let initials = [firstInitials,secoInitials].toString()
+    return initials.replace(/,/g, '.')
 }
+
+console.log(abbrevName("sam harris"));
