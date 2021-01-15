cd12efb82c9f6d068decda4df6998acce83c30e1
johnbelmdev <johnbel458@gmail.com>
2021-01-15 16:24:38 -0500

    new codewars

---
+++ 8_Kyu /Find Jon Snow Parents/index.js
@@ -0,0 +1,16 @@
+// create a function that takes in two parameters
+// Jon's snow's dad is equal to Rhaegar Targaryen and mom is equal to  Lyanna Stark.
+// example
+        // if ned stark is the dad parameters and the mom is cersei
+
+function jonSnowParents(dad, mom){
+//your code
+    if (dad === "Rhaegar Targaryen" && mom === "Lyanna Stark"){
+      return "Jon Snow you deserve the throne"
+    }
+    else  {
+          return "You know nothing, Jon Snow"
+    }
+
+}
+console.log(jonSnowParents());
