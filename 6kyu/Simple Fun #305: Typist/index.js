a951400ab84df8251eca69a1c5f0078fc3242ee3
johnbelmdev <johnbel458@gmail.com>
2021-01-21 21:58:34 -0500

    commit

---
+++ 6kyu/Simple Fun #305: Typist/index.js
@@ -0,0 +1,40 @@
+// the input I'm getting that I'm going to be getting will be
+// be a string
+// Return the number that John tapped the keyboard
+//find the time that he press the keyboard
+// s = 'a' => 1
+// s = 'aa' => 2
+// s = "A" => 2
+// s = "Aa" 4 => pressing caps is also included 1 for cap being
+// press increment by 1 now that's 3 press a now that's 4
+// define a counter that check time a certain letter has been pressed
+// for every letter that's a capital will be two
+//first split all the strings that are being passed
+//aaccjjHHHHFNNNkkkHHH
+//AA => 3
+
+
+function typist(s){
+  //coding and coding..
+   let splitString = s.split('')
+   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+   // console.log(alphabet);
+   let count = 0;
+   let uppCase = 1;
+
+  for (var i = 0; i < splitString.length; i++) {
+    count+=1
+    if(alphabet.includes(splitString[i])){
+      console.log(splitString[i])
+      uppCase+=1
+    }
+
+
+  }
+  return uppCase
+
+   // console.log(upperCaseSplit);
+}
+
+console.log(typist("aaAA"));
+// console.log(typist("A"));
---
+++ 8_Kyu /Brownrw8's "for" loop/index.js
@@ -0,0 +1,8 @@
+// fix the for-loop below to ensure the array 'nums'
+// contains the numbers 1-5
+
+var nums = [];
+
+for(var i=1;i<=5;i++){
+  nums.push(i);
+}
