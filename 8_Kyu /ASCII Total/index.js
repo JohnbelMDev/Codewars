56dab1bc3a0cb90cae5834c4216f502667a526a0
johnbelmdev <johnbel458@gmail.com>
2021-02-23 00:56:32 -0500

    commit

--- 8_Kyu /ASCII Total/index.js
+++ 8_Kyu /ASCII Total/index.js
@@ -1,5 +1,8 @@
+//get total 
+
 function uniTotal(a)
-{ let splitString = a.split('')
+{
+  let splitString = a.split('')
   let total = 0;
     for (let i = 0; i < splitString.length; i++){
       total += splitString[i].charCodeAt(0)
