148dc81771f55e4ce8f898a0945d8ceb08341b7b
johnbelmdev <johnbel458@gmail.com>
2021-02-25 12:54:39 -0500

    noOdds

---
+++ 7_kyu/No oddities here/index.js
@@ -0,0 +1,14 @@
+function noOdds( values ){
+  // Return all non-odd values
+
+  let evenNumbers = []
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] % 2 === 0){
+       evenNumbers.push(values[i])
+    }
+  }
+  return evenNumbers
+
+}
+
+console.log(noOdds([0,1,2,3]));
