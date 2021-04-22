ede00f5ba96b092dae79029ad04b94c4622cadc9
johnbelmdev <johnbel458@gmail.com>
2021-04-21 21:07:49 -0400

    commit

---
+++ 7_kyu/Multiply array values and filter non-numeric/index.js
@@ -0,0 +1,16 @@
+// function multiplyAndFilter(array, multiplier){
+//    let newArray = []
+//      for (var i = 0; i < array.length; i++) {
+//         if(typeof(array[i]) === 'number'){
+//             array[i] = array[i] * multiplier
+//         }
+//      }
+//      return arrayp[i]
+//
+// }
+
+for (var i = 0; i < 100; i++) {
+  console.log('Babeeee');
+}
+
+// console.log(multiplyAndFilter([1,2,'he',[],3,4], 1.5));
---
+++ 8_Kyu /Regex count lowercase letters/index.js
@@ -0,0 +1,8 @@
+function lowercaseCount(str){
+    //How many?
+    var countLowerCase = str.replace(/[^a-z]/g, '').length;
+    return countLowerCase
+    console.log(numUpper);
+
+}
+console.log(lowercaseCount("abc"));
