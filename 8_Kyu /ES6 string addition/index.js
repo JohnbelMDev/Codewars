4468bb1cdff944df46343b47f44a72c5ca4c39ab
johnbelmdev <johnbel458@gmail.com>
2021-04-27 15:23:54 -0400

    commit

--- 8_Kyu /ES6 string addition/index.js
+++ 8_Kyu /ES6 string addition/index.js
@@ -4,14 +4,15 @@ function joinStrings(string1, string2){
    let x = temp.toString()
    let count = (x.match(/,/g) || []).length
    if( count == 1){
-console.log('3');
+// console.log('3');
      return x.replace(',', ' ')
    }
    else {
      // console.log('Hi');
      let remove = x.replace(',', '')
-     console.log(remove);
-     return remove
+
+     // console.log(remove.replace(/,/g, ', '));
+     return remove.replace(/,/g, ', ')
    }
    // console.log(count); //logs 3
 
