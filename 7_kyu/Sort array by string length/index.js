762f7064d28593f116eba3a6c5872a76ec876d49
johnbelmdev <johnbel458@gmail.com>
2021-04-29 16:07:16 -0400

    commit

--- 7_kyu/Sort array by string length/index.js
+++ 7_kyu/Sort array by string length/index.js
@@ -1,5 +1,12 @@
+// https://www.codewars.com/kata/57ea5b0b75ae11d1e800006c/train/javascript
+
 function sortByLength (array) {
   // Return an array containing the same strings, ordered from shortest to longest
+  let sortByLength = [];
+  let sort = []
+
+return array.sort((a, b) =>  a.length - b.length)
+      // console.log(array);
 };
 
-console.log(sortByLength());
+console.log(sortByLength(["Telescopes", "Glasses", "Eyes", "Monocles"]));
