074223725e10eb688e30d71a62e1aaeb06290e6b
johnbelmdev <johnbel458@gmail.com>
2021-05-11 22:49:58 -0400

    commit

---
+++ 6kyu/The Walker/index.js
@@ -0,0 +1,20 @@
+// toA is alpha and is 45 degree
+// hAB is beta and it's 30 degrees
+// uBc is gamma and it's 60 degrees
+// OA is the line in the graph
+// AB is the line in the graph
+// AB is the line in the graph
+
+// learn about measurements
+// minutes and angles
+//
+
+function solve(a, b, c, alpha, beta, gamma) {
+    // your code
+}
+
+
+
+// resources
+
+// https://www.google.com/search?q=minutes+in+angles&oq=minutes+in+angles&aqs=chrome..69i57j33i10i160.1653j0j7&sourceid=chrome&ie=UTF-8
