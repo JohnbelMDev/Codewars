9ab8a7b8579e7837e5873ef882cb43f2e7d5d738
johnbelmdev <johnbel458@gmail.com>
2021-06-02 22:42:07 -0400

    commit

--- 7_kyu/Basic Math (Add or Subtract).js
+++ 7_kyu/Basic Math (Add or Subtract).js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/5809b62808ad92e31b000031/solutions/javascript
 // codewars
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/eval
 
 function calculate(str) {
 //your code here...
