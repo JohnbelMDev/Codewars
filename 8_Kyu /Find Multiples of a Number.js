2b6f02ffd8180b93b9c7953e1057890e6ea5420a
johnbelmdev <johnbel458@gmail.com>
2021-07-29 15:49:59 -0400

    commit

--- 8_Kyu /Find Multiples of a Number.js
+++ 8_Kyu /Find Multiples of a Number.js
@@ -5,6 +5,14 @@
 
 function findMultiples(integer, limit) {
   //your code here
+let multiples = []
+  for (let i = integer; i <= limit; i++) {
+
+       if(i % integer === 0 ){
+          multiples.push(i)
+       }
+  }
+  return multiples
 }
 
 console.log(findMultiples(2,6));
