8caa60a4bc21a8c9040a156c94a1f21238aee735
johnbelmdev <johnbel458@gmail.com>
2021-07-29 00:38:29 -0400

    commit

---
+++ 8_Kyu /Find Multiples of a Number.js
@@ -0,0 +1,10 @@
+// params: numbers
+//return the list of multiples between both numbers
+
+//codewars https://www.codewars.com/kata/58ca658cc0d6401f2700045f/train/javascript
+
+function findMultiples(integer, limit) {
+  //your code here
+}
+
+console.log(findMultiples(2,6));
